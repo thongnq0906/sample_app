8141e85e6fd1d2482e3676bc0a4dbdd279f754f1
thongnq0906 <thongnq0906@gmail.com>
2019-02-19 14:57:44 +0700

    c7

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  root 'static_pages#home'
-  get 'help', to: 'static_pages#help', as: 'help'
-  get 'about', to: 'static_pages#about', as: 'about'
-  get 'signup', to: 'users#new', as: 'signup'
+  get "users/new"
+  root "static_pages#home"
+  get "help", to: "static_pages#help", as: "help"
+  get "about", to: "static_pages#about", as: "about"
+  get "signup", to: "users#new", as: "signup"
+  post "/signup",  to: "users#create"
+  resources :users
 end
