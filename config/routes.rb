4b4a6941a6f93a2d5c1acff9c88c4cba7bf60539
thongnq0906 <thongnq0906@gmail.com>
2019-02-19 17:23:43 +0700

    c8

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get "users/new"
   root "static_pages#home"
   get "help", to: "static_pages#help", as: "help"
   get "about", to: "static_pages#about", as: "about"
   get "signup", to: "users#new", as: "signup"
   post "/signup",  to: "users#create"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   resources :users
 end
